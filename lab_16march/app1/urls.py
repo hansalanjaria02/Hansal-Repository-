175e88510687ae8ea14d2ba82fa3a7e4da773c9f
Hansal Kaushangkumar Anjaria <hansal.anjaria@gmail.com>
2026-03-16 10:50:19 +0530

    Delete lab_16march directory

--- lab_16march/app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.student_list, name='student_list'),          # Home page (List)
-    path('add/', views.student_create, name='student_create'),  # Naya student
-    path('edit/<int:pk>/', views.student_update, name='student_update'), # Edit
-    path('delete/<int:pk>/', views.student_delete, name='student_delete'), # Delete
-]
